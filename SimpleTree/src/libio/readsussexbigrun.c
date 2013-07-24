76c063b92d9d2e6ee2eb7d4ec005684fbd5f1792
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-07-24 13:57:21 +0700

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -73,8 +73,9 @@ m_halo_wrapper_t sussexbigrun_filterhalos_and_particles(m_halo_wrapper_t mhalo)
   for(ihalo=0;ihalo < mhalo.nHalos; ihalo++)
     {
       printf("ihalo: %llu\n",ihalo);
-      insert.haloID = mhalo.mhalos[ihalo].ID;
+
       tmp[0].npart += mhalo.mhalos[ihalo].npart;
+      printf("tot npart:%llu\n",tmp[0].npart);
       tmp[0].mparticle = memmgr_realloc(tmp[0].mparticle,sizeof(m_particle_t)*tmp[0].npart,sizeof(m_particle_t)*(tmp[0].npart-mhalo.mhalos[ihalo].npart),memmgr_buff);
       for(ipart=tmp[0].npart-mhalo.mhalos[ihalo].npart-1;ipart<tmp[0].npart;ipart++)
 	{
