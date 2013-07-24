eee85eb0db0f3c7d599f3b46a5e1ebd4be84af81
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-07-25 05:19:02 +0700

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -111,7 +111,7 @@ m_halo_wrapper_t sussexbigrun_filterhalos_and_particles(m_halo_wrapper_t mhalo)
 	      key_for_sort[jpart].order = jpart;
 	    }
 	  qsort(key_for_sort,mhalo.mhalos[h_target].npart, sizeof(key_sort_t),compare_m_halo_t_by_ID);
-	  p_target = search_key_sort_t_array_for_ID( tmp[0].mparticle[ipart].ID, mhalo.mhalos[h_target].npart , key_for_sort);
+	  p_target = search_key_sort_t_for_ID( tmp[0].mparticle[ipart].ID, mhalo.mhalos[h_target].npart , key_for_sort);
 	  p_target = key_for_sort[p_target].order;
 	  memmgr_free(key_for_sort,mhalo.mhalos[h_target].npart*sizeof(key_sort_t),memmgr_buff);
 	  //p_target = search_particlelist_t_for_ID();
