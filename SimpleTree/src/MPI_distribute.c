78bfef875cccd7f6a247c8777a26360cac221d9d
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-07-25 09:27:49 +0700

    autoupdate

--- SimpleTree/src/MPI_distribute.c
+++ SimpleTree/src/MPI_distribute.c
@@ -1,6 +1,6 @@
 #include "MPI_distribute.h"
 
-void MPI_distribute_remove_duplicate_with_structure_level(m_halo_wrapper_t* mhalo)
-{
+//void MPI_distribute_remove_duplicate_with_structure_level(m_halo_wrapper_t* mhalo)
+//{
 
-}
+//}
