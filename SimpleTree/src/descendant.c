43d872a7b646e3120644c52a86417756f85cd571
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-07-30 05:53:13 +0700

    autoupdate

--- SimpleTree/src/descendant.c
+++ SimpleTree/src/descendant.c
@@ -57,8 +57,8 @@ void make_link_AB(m_halo_wrapper_t* haloA, m_halo_wrapper_t* haloB)
       for(ipart=0; ipart < haloA->mhalos[ihalo].npart; ipart++)
 	{
 	  curpart = haloA->mhalos[ihalo].Particles[ipart].ID;
-	  printf("outside: search for %llu\n",curpart);
-	  //ihid =  search_m_particle_t_for_ID(curpart,tmppart[0].npart,tmppart[0].mparticle );
+	  //printf("outside: search for %llu\n",curpart);
+	  ihid =  search_m_particle_t_for_ID(curpart,tmppart[0].npart,&(tmppart[0].mparticle) );
 	}
     }
 }
