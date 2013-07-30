8ebb06d83ac8b908e1b216dbece3579d78e43197
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-07-30 17:31:25 +0700

    autoupdate

--- SimpleTree/src/descendant.c
+++ SimpleTree/src/descendant.c
@@ -48,7 +48,11 @@ void make_link_AB(m_halo_wrapper_t* haloA, m_halo_wrapper_t* haloB)
    
     }
   sprintf(memmgr_buff,"Particle inside wrapper: Hash");
-  qsort(tmppart[0].mparticle,tmppart[0].npart, sizeof(m_particle_t),compare_m_particle_t_by_ID);
+  qsort(tmppart[0].mparticle, tmppart[0].npart, sizeof(m_particle_t),compare_m_particle_t_by_ID);
+  for(ipart=0; ipart<tmppart[0].npart; ipart++ )
+    {
+      printf("ipart: %llu => %llu\n",ipart,tmppart[0].mparticle[ipart].ID);
+    }
   old = tmppart[0].npart*sizeof(m_particle_t);
   new = countpart*sizeof(m_particle_t);
   tmppart[0].mparticle = memmgr_realloc(tmppart[0].mparticle,new,old,memmgr_buff);
