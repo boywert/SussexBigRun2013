6192cf4bdfbc23f5e2e5440dc438444805ff0794
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-07-31 03:54:27 +0700

    autoupdate

--- SimpleTree/src/common.c
+++ SimpleTree/src/common.c
@@ -5,9 +5,9 @@ const uint64_t NULLPOINT = (uint64_t)-1;
 const uint64_t MAXUSEABLE = (uint64_t)-2;
 const double max_part_speed_in_c = 0.01;
 
-void initialise_MPI(int argc,char **argv)
+void initialise_MPI()
 {
-  MPI_Init ( &argc, &argv );
+  MPI_Init  ();
   MPI_Comm_size ( MPI_COMM_WORLD, &mpi_nodes);
   MPI_Comm_rank ( MPI_COMM_WORLD, &mpi_rank );
 }
