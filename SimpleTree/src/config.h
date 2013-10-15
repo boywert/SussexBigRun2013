53f4dc63264c69ee273ab9ef6bf343ce2b5e445a
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-15 03:07:45 +0100

    autoupdate

--- SimpleTree/src/config.h
+++ SimpleTree/src/config.h
@@ -4,7 +4,7 @@
 #define npart_box 5159780352
 #define boxsize 47000.0
 #define domain_per_dim 6
-#define INPUTDIR "/mnt/lustre/scratch/cs390/Sussexbigrun2013/cubepm_130315_6_1728_47Mpc_ext2/results"
-#define OUTPUTDIR "/mnt/lustre/scratch/cs390/Sussexbigrun2013/cubepm_130315_6_1728_47Mpc_ext2/mergertree"
+#define INPUTDIR /mnt/lustre/scratch/cs390/Sussexbigrun2013/cubepm_130315_6_1728_47Mpc_ext2/results
+#define OUTPUTDIR /mnt/lustre/scratch/cs390/Sussexbigrun2013/cubepm_130315_6_1728_47Mpc_ext2/mergertree
 
 #endif
