094a664b23ea409b4bb7e47dfb2cb678f1a34f85
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-20 02:21:38 +0100

    autoupdate

--- SimpleTree/tools/convertSUSSING2LGAL.py
+++ SimpleTree/tools/convertSUSSING2LGAL.py
@@ -1,13 +1,15 @@
 import numpy
 
-AHFdir = "/export/data/virgo/SUSSING2013_DATA/datasetI/"
-AHFprefix = "62.5_dm_"
+AHFdir = "/export/data/virgo/SUSSING2013_DATA/datasetI"
+AHFprefix = "62.5_dm"
 SUSSINGtree = ""
 SNAPfile = "/export/data/virgo/SUSSING2013_DATA/datasetI/data_snaplist.txt"
 
 def readAHFascii(SNAPfile,AHFdir,AHFprefix):
     timesnap = numpy.loadtxt(SNAPfile)
+    halocat = {}
     for time in timesnap:
-        print time
+        filename = "%s/%s_%03d.z%3.3f.AHF_halos" % (AHFdir, AHFprefix, time[0], time[2])
+        print filename
 
 readAHFascii(SNAPfile,AHFdir,AHFprefix)
