76dcaaba7ad1814d37aacd65f17794470ba79be3
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-20 03:51:43 +0100

    autoupdate

--- SimpleTree/tools/convertSUSSING2LGAL.py
+++ SimpleTree/tools/convertSUSSING2LGAL.py
@@ -5,38 +5,26 @@ AHFdir = "/export/data/virgo/SUSSING2013_DATA/datasetI"
 AHFprefix = "62.5_dm"
 SUSSINGtree = ""
 SNAPfile = "/export/data/virgo/SUSSING2013_DATA/datasetI/data_snaplist.txt"
-def properround(number,digits):
-    strfmt = "%."+str(digits+1)+"f"
-    strnum = strfmt % (number)
-    if strnum[len(strnum)-1] == "5":
-        if long(strnum[len(strnum)-2])%2 == 0:
-            strout = strnum[0:(len(strnum)-1)]
-        else:
-            strnum[len(strnum)-2] = str(long(strnum[len(strnum)-2])+1)
-            strout = strnum[0:(len(strnum)-1)]
-    else:
-        strfmt = "%."+str(digits)+"f"
-        strout = strfmt % (number)
-    return strout
+
 def readAHFascii(SNAPfile,AHFdir,AHFprefix):
     timesnap = numpy.loadtxt(SNAPfile)
     halocat = {}
     for time in timesnap:
         zstring = "%.3f" % (time[2])
-        print zstring[len(zstring)-1]
+        #print zstring[len(zstring)-1]
         filename = "%s/%s_%03d.z%s.AHF_halos" % (AHFdir, AHFprefix, time[0], zstring)
-        print "checking "+filename
+        #print "checking "+filename
         if os.path.isfile(filename) == False:
             zstring = "%.3f" % (time[2]+0.00001)
-            print zstring[len(zstring)-1] 
+            #print zstring[len(zstring)-1] 
             filename = "%s/%s_%03d.z%s.AHF_halos" % (AHFdir, AHFprefix, time[0], zstring)
         if os.path.isfile(filename) == False:
             zstring = "%.3f" % (time[2]-0.00001)
-            print "checking "+filename
-            print zstring[len(zstring)-1] 
+            #print "checking "+filename
+            #print zstring[len(zstring)-1] 
             filename = "%s/%s_%03d.z%s.AHF_halos" % (AHFdir, AHFprefix, time[0], zstring)
         if os.path.isfile(filename) == False:
-            print "checking "+filename
+            print "checking error"+filename
             exit()
         print "Reading "+filename
         stat = os.stat(filename)
