78158d28c9b334ce588493b4526f0b48ce2ebf08
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-22 07:10:41 +0100

    autoupdate

--- SimpleTree/tools/convertSUSSING2LGAL.py
+++ SimpleTree/tools/convertSUSSING2LGAL.py
@@ -202,25 +202,18 @@ def outputtrees(halocat2):
             halo = halocat[hid]
             if(halo["MainHalo"] == -1):
                 halo["MainHalo"] = halo["ID"]
-            if halo["Descendant"] not in maptree:
-                print halo["Descendant"]
             buffer = struct.pack("i",int(maptree[halo["Descendant"]]))
             fp.write(buffer)
-            if halo["FirstProgenitor"] not in maptree:
-                print halo["FirstProgenitor"]
             buffer = struct.pack("i",int(maptree[halo["FirstProgenitor"]]))
             fp.write(buffer)
-            if halo["NextProgenitor"] not in maptree:
-                print halo["NextProgenitor"]
             buffer = struct.pack("i",int(maptree[halo["NextProgenitor"]]))
             fp.write(buffer)
             if halo["MainHalo"] not in maptree:
-                print halo
-                print halocat[halo["MainHalo"]]
+                halo["MainHalo"] = halo["ID"]
             buffer = struct.pack("i",int(maptree[halo["MainHalo"]]))
             fp.write(buffer)
             if halo["NextHalo"] not in maptree:
-                print halo["NextHalo"]
+                halo["NextHalo"] = -1
             buffer = struct.pack("i",int(maptree[halo["NextHalo"]]))
             fp.write(buffer)
             buffer = struct.pack("i",halo["Len"])
