663c5b7df4a6d6e404af9cf16f68c15ace8af559
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-23 02:38:06 +0100

    autoupdate

--- SimpleTree/tools/convertSUSSING2LGAL2.py
+++ SimpleTree/tools/convertSUSSING2LGAL2.py
@@ -205,7 +205,7 @@ def outputtrees(halocat2):
                 for hid in fulltree[tree]:
                     halo = halocat[hid]
                     if halo["MainHalo"] not in maptree: # -1 is in maptree
-                        print "change ",halo["ID"],"=>",halo["MainHalo"]
+                        print "change mainhalo ",halo["ID"],"=>",halo["MainHalo"]
                         target = halo["MainHalo"]
                         oldtree = halocat[target]["TreeNr"]
                         if(oldtree > -1):
@@ -219,7 +219,7 @@ def outputtrees(halocat2):
                         break
                     
                     if halo["NextHalo"] not in maptree: # -1 is in maptree
-                        print "change ",halo["ID"],"=>",halo["NextHalo"]
+                        print "change nexthalo ",halo["ID"],"=>",halo["NextHalo"]
                         target = halo["NextHalo"]
                         oldtree = halocat[target]["TreeNr"]
                         if(oldtree > -1):
