94e5fd57da0f4461d38a8360d9f5db964ab9cc0b
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-29 02:20:48 +0000

    autoupdate

--- SimpleTree/src/chunk2cubep3m.c
+++ SimpleTree/src/chunk2cubep3m.c
@@ -77,7 +77,7 @@ int main(int argc,char **argv)
 	}
       for(l=0;l<pow3(chunk_per_dim);l++)
 	{
-	  printf("Redshift: %f\n", snap1);
+	  if(mpi_rank == 0) printf("Redshift: %f\n", snap1);
 	  if(l%mpi_nodes == mpi_rank)
 	    {
 	      printf("\treading chunk %d by rank:%d\n",l,mpi_rank);
