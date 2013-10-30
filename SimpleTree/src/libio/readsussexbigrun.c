b62985743a28f5c92b146dcc7441fdf54cc1fe3f
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-10-30 11:49:23 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -657,6 +657,7 @@ make_catalogue_halo_wrapper_t sussexbigrun_load_halo_catalogue_binary_single_chu
   shift_X = chunk_X*param_boxsize/param_chunk_per_dim - 2.*param_buffer_size*param_boxsize/param_domain_per_dim;
   shift_Y = chunk_Y*param_boxsize/param_chunk_per_dim - 2.*param_buffer_size*param_boxsize/param_domain_per_dim;
   shift_Z = chunk_Z*param_boxsize/param_chunk_per_dim - 2.*param_buffer_size*param_boxsize/param_domain_per_dim;
+  printf("chunk: %d,%d,%d\n",chunk_X,chunk_Y,chunk_Z);
   printf("shift: %f,%f,%f\n",shift_X,shift_Y,shift_Z);
 
   lowerbound[0] = chunk_X*param_boxsize/param_chunk_per_dim;
