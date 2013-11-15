e9c90cda6099ce52de2b05f4c4b87e9c07be7eba
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-15 01:03:02 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -752,9 +752,7 @@ make_catalogue_halo_wrapper_t sussexbigrun_read_AHF_binary_from_raw(FILE *fphalo
   ReadULong(fphalo, &numHalos,   swap);
   ReadUInt (fphalo, &numColumns, swap);
   //printf("halofile: nhalo = %llu\n",numHalos);
-  ReadULong(fpprof, &numHalos,   swap);
-  ReadUInt (fpprof, &numColumns, swap);
-  printf("numcolumn = %d\n",(int) numColumns);
+
 
   sprintf(memmgr_buff,"Halo Array");
 
