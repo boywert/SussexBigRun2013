9a1db9a2e5505d287e7a2c2832248964194d0470
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-24 01:25:46 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -1081,7 +1081,9 @@ make_catalogue_halo_wrapper_t sussexbigrun_output_cubep3m(make_catalogue_halo_wr
   MPI_Barrier(MPI_COMM_WORLD);
 
   open_cubep3m_for_writing(ndomains, chalo.redshift, domain_contained);
+  MPI_Barrier(MPI_COMM_WORLD);
   close_cubep3m_for_writing(ndomains);
+  MPI_Barrier(MPI_COMM_WORLD);
   free(domain_contained);
   return chalo;
 }
