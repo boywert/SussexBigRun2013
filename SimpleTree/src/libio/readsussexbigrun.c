6041552625a55d20b8707ff3fd7792e6a46b0fef
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-24 02:44:02 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -1126,8 +1126,8 @@ void open_cubep3m_for_writing(int ndomains, float redshift, int *domain_containe
     {
       /* halos_bin */
       sprintf(sbuf,"%s/z_%2.3f/%2.3f_AHF_halos_cubepm_domain_%d_halos.dat_bin",param_CUBEP3MOUT,redshift,redshift,domain_contained[ifile]);
-      /* cubep3m_save_halos_file[ifile] = fopen(sbuf,"rwb+"); */
-      /* fclose(cubep3m_save_halos_file[ifile]); */
+      &(cubep3m_save_halos_file[ifile]) = fopen(sbuf,"rwb+");
+      fclose(&(cubep3m_save_halos_file[ifile])); 
       /* /\* write header *\/ */
       /* fwrite(&one,sizeof(int32_t),1,cubep3m_save_halos_file[ifile]); */
       /* fwrite(&zero,sizeof(uint64_t),1,cubep3m_save_halos_file[ifile]); */
