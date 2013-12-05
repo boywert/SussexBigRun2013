bef64a126945a1ef3b59b637842fccc375592696
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-12-05 11:33:52 +0000

    autoupdate

---
+++ AHF_halos/run_all.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+#$ -N boyd
+#$ -M cs390@sussex.ac.uk
+#$ -m bea
+#$ -j y
+#$ -cwd
+#$ -pe openmpi 216 #eg12-36
+#$ -q mps_amd.q
+#$ -S /bin/bash
+# source modules environment:
+module add sge
+
+mpi_chunk=216
+mpi_ahf=8
+
+cubep3m_boxsize=47
+cubep3m_mesh=3456
+cubep3m_node=6
+
+pid_flag=1
+buffer_size=1.5
+drho=200
+n_chunks_pd=3
+n_chunks_total=27
+last_chunk=26
+
+workspace="/home/c/cs/cs390/SussexBigRun2013/AHF_halos/"
+particle_folder="/research/prace/cubepm_131025_6_1728_47Mpc_ext2/results/"
+ahf_folder="/home/c/cs/cs390/SussexBigRun2013/ahf-v1.0-056.SUSSEXBIGRUN/"
+ahf_exec="/home/c/cs/cs390/SussexBigRun2013/ahf-v1.0-056.SUSSEXBIGRUN/bin/AHF-v1.0-056"
+chunk_srcfolder="/home/c/cs/cs390/SussexBigRun2013/Chunking/"
+chunk_exec="/home/c/cs/cs390/SussexBigRun2013/Chunking/chunk"
+chunk_folder="/mnt/lustre/scratch/cs390/tmp/cubepm_131025_6_1728_47Mpc_ext2/chunked_output/"
+ahfoutput_folder="/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131025_6_1728_47Mpc_ext2/"
+
+ahf_template="/home/c/cs/cs390/SussexBigRun2013/AHF_halos/AHF.input-template"
+cubep3minfo="/home/c/cs/cs390/SussexBigRun2013/AHF_halos/cubep3m.info"
+
+snaplist="/home/c/cs/cs390/SussexBigRun2013/AHF_halos/snaplist"
+
+#compile things
+cd ${ahf_folder}
+make clean
+make 
+cd ${chunk_srcfolder}
+make clean
+make 
+
+last_redshift="1000.0"
+
+echo '' > $snaplist
+while read line
+do
+    redshift=$(printf '%3.3f' $line)
+    echo "redshift = " $redshift
+    firstfile=$(printf '%s/%sxv0.dat' $particle_folder $redshift)
+    #make folder prepared for chunking
+    this_workspace=$(printf '%s/z_%s_%d/' $workspace $redshift $drho)
+    mkdir -p $this_workspace
+    for i in $(seq 0 $last_chunk)
+    do
+	this_chunkfolder=$(printf '%s/z_%s/chunk_%d/' $chunk_folder $redshift $i)
+	mkdir -p $this_chunkfolder
+	this_output_prefix=$(printf '%s/z_%s_%d/chunk_%d/' $ahfoutput_folder $redshift $drho $i)
+	mkdir -p $this_output_prefix
+    done
+ 
+    cd ${this_workspace}
+    this_chunk_param=$(printf 'chunk_param_%s' $redshift)
+
+    echo ${redshift} > ${this_chunk_param}
+    echo "dummy" >>  $this_chunk_param
+    echo $particle_folder >> $this_chunk_param
+    echo $chunk_folder >> $this_chunk_param
+    echo $cubep3m_boxsize >> $this_chunk_param
+    echo $cubep3m_node >> $this_chunk_param
+    echo $cubep3m_mesh >> $this_chunk_param
+    echo $pid_flag >> $this_chunk_param
+    echo $buffer_size >> $this_chunk_param
+    echo $n_chunks_pd >> $this_chunk_param
+    echo $n_chunks_pd >> $this_chunk_param
+    echo $n_chunks_pd >> $this_chunk_param
+    if [ -e $firstfile ] 
+    then
+	# Chunk cubep3m
+	this_pbs=$(printf 'chunking_%s.pbs' $redshift)
+	chunk_job_name=$(printf 'chunking_%s' $redshift)
+	echo "#!/bin/bash" > $this_pbs
+	echo "#$ -N" $chunk_job_name >> $this_pbs
+	echo "#$ -M cs390@sussex.ac.uk" >> $this_pbs
+	echo "#$ -m bea" >> $this_pbs
+	echo "#$ -j y" >> $this_pbs
+	echo "#$ -cwd" >> $this_pbs
+	echo "#$ -pe openmpi" $mpi_chunk >> $this_pbs 
+	echo "#$ -q mps_amd.q" >> $this_pbs
+	echo "#$ -S /bin/bash" >> $this_pbs
+	echo "module add sge" >> $this_pbs
+	echo 'mpirun -np' $mpi_chunk $chunk_exec $this_chunk_param >> $this_pbs
+	cat $this_pbs
+	qsub $this_pbs
+	# run AHF on every chunks
+	# cubep3m
+	cp $cubep3minfo $this_cubep3m_info
+	
+	for i in $(seq 0 $last_chunk)
+	do
+	    cd $this_workspace
+	    this_pbs=$(printf 'ahf_chunk_%d' $i)
+	    this_ahf_config=$(printf 'ahf_config_%d' $i)
+	    this_cubep3m_info="cubep3m.info"
+	    # ahf input file
+	    cp ${ahf_template} ${this_ahf_config}
+	    this_ic_filename=$(printf '%s/z_%s/chunk_%d/%sxv_chunk_%d_' $chunk_folder $redshift $i $redshift $i)
+	    this_output_prefix=$(printf '%s/z_%s_%d/chunk_%d/%s_' $ahfoutput_folder $redshift $drho $i $redshift)
+	    echo 'ic_filename=' $this_ic_filename  >> $this_ahf_config
+	    echo 'outfile_prefix=' $this_output_prefix >> $this_ahf_config
+	    # pbs file
+	    this_pbs=$(printf 'ahf_%s_%d.pbs' $redshift $i)
+	    ahf_job_name=$(printf 'ahf_%s_%d' $redshift $i)
+	    echo "#!/bin/bash" > $this_pbs
+	    echo "#$ -N" $ahf_job_name >> $this_pbs
+	    echo "#$ -M cs390@sussex.ac.uk" >> $this_pbs
+	    echo "#$ -m bea" >> $this_pbs
+	    echo "#$ -j y" >> $this_pbs
+	    echo "#$ -cwd" >> $this_pbs
+	    echo "#$ -hold_jid" $chunk_job_name >> $this_pbs
+	    echo "#$ -pe openmpi" $mpi_ahf >> $this_pbs 
+	    echo "#$ -q mps_amd.q" >> $this_pbs
+	    echo "#$ -S /bin/bash" >> $this_pbs
+	    echo "module add sge" >> $this_pbs
+	    echo 'mpirun -np' $mpi_ahf $ahf_exec $this_ahf_config >> $this_pbs
+	    cat $this_pbs
+	    qsub $this_pbs
+	done
+	echo $redshift >> $snaplist
+    fi
+    rm -rf ${chunk_folder}/z_${redshift}/*
+done < halofinds
+##mpirun -np 8 ../bin/AHF-v1.0-056 AHF.input-template2
