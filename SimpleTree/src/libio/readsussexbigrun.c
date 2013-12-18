2f739b885006f1b8abbac2f6498d5e53d5ee51dc
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-12-18 11:28:57 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -428,7 +428,7 @@ m_halo_wrapper_t sussexbigrun_read_AHF_binary(FILE *fphalo, FILE *fppart, FILE *
   char memmgr_buff[memmgr_max_str];
   struct particle_buffer *pid_buff;
 
-  define SKIP(&fp,byte) {fseek(&fd, byte, SEEK_CUR);}
+#define SKIP(&fp,byte) {fseek(&fd, byte, SEEK_CUR);}
 
   if(fphalo == NULL || fppart == NULL) 
     {
@@ -478,10 +478,10 @@ m_halo_wrapper_t sussexbigrun_read_AHF_binary(FILE *fphalo, FILE *fppart, FILE *
   counthalo = mhalo.nHalos;
   counthalo_local = 0;
   old_nHalos = mhalo.nHalos;
-  old = chalo.nHalos*sizeof(m_halo_t);
+  old = mhalo.nHalos*sizeof(m_halo_t);
   new = old + numHalos*sizeof(m_halo_t);
   chalo.nHalos += numHalos;
-  new = chalo.nHalos*sizeof(m_halo_t);
+  new = mhalo.nHalos*sizeof(m_halo_t);
   
   mhalo.mhalos = memmgr_realloc(mhalo.mhalos,new,old,memmgr_buff);
   flag = 0;
