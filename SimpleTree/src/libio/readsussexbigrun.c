e475378c0884f41fd66e1d8d22b0d2457433e39a
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-12-19 00:33:39 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -862,9 +862,9 @@ make_catalogue_halo_wrapper_t sussexbigrun_load_halo_catalogue_binary_single_chu
       sprintf(partfile,"%s/z_%2.3f_200/chunk_%d/%2.3f_.%04d.z%2.3f.AHF_particles_bin",folder,redshift,chunk,redshift,i,redshift);
       sprintf(halofile,"%s/z_%2.3f_200/chunk_%d/%2.3f_.%04d.z%2.3f.AHF_halos_bin",folder,redshift,chunk,redshift,i,redshift);
       sprintf(proffile,"%s/z_%2.3f_200/chunk_%d/%2.3f_.%04d.z%2.3f.AHF_profiles_bin",folder,redshift,chunk,redshift,i,redshift);
-      /* printf("reading %s\n",partfile); */
-      /* printf("reading %s\n",halofile); */
-      /* printf("reading %s\n",proffile); */
+      printf("reading %s\n",partfile); 
+      printf("reading %s\n",halofile); 
+      printf("reading %s\n",proffile); 
       fphalo = fopen(halofile,"rb");
       fppart = fopen(partfile,"rb");  
       fpprof = fopen(proffile,"rb"); 
