f70edaf617bac231a138d5b1d499d114905d3181
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-01-14 06:46:55 +0000

    autoupdate

--- SimpleTree/src/maketree.c
+++ SimpleTree/src/maketree.c
@@ -119,7 +119,7 @@ int main(int argc,char **argv)
 		{
 		  printf("in main after readA: %llu npart:%llu\n",halocatB[0].mhalos[ihalo].ID,halocatB[0].mhalos[ihalo].npart);
 		}
-	      make_link_AB(&(halocatA[0]),&(halocatB[0]), dt*kpc2m);
+	      //make_link_AB(&(halocatA[0]),&(halocatB[0]), dt*kpc2m);
 
 	      free_m_halo_wrapper(halocatA);
 
