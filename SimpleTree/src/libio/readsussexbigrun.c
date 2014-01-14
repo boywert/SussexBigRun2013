8cc6c748a702d313c832a33dd54f87808ec61c8e
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-01-14 06:32:18 +0000

    autoupdate

--- SimpleTree/src/libio/readsussexbigrun.c
+++ SimpleTree/src/libio/readsussexbigrun.c
@@ -261,7 +261,7 @@ m_halo_wrapper_t sussexbigrun_load_halo_catalogue_binary_single_domain_include_b
   		    position = 5;
   		  if(k==-1)
   		    position = 6;
-  		  mhalo = sussexbigrun_add_halo_buffer_binary(folder, redshift, domain, domain_width, domain_per_dim, dx+fixed_buffer, position, mhalo);
+  		  //mhalo = sussexbigrun_add_halo_buffer_binary(folder, redshift, domain, domain_width, domain_per_dim, dx+fixed_buffer, position, mhalo);
   		}
   	    }
   	}
