a67bcdf07412e8a20101a0798524c28c4d7e51e3
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-01-16 05:15:31 +0000

    autoupdate

--- SimpleTree/src/treeformat.c
+++ SimpleTree/src/treeformat.c
@@ -7,7 +7,7 @@ void allocate_proglist(m_halo_wrapper_t* ptr)
     {
       for(i=0;i<ptr[j].nHalos;i++)
 	{
-	  ptr[j].mhalos[i].proglist = malloc(sizeof(uint64_t));
+	  ptr[j].mhalos[i].proglist = malloc(0);
 	}
 
     }
@@ -18,11 +18,12 @@ void free_m_halo_wrapper(m_halo_wrapper_t* ptr)
   char buff[memmgr_max_str];
   for(j=0;j<1;j++)
     {
-      
       sprintf(buff,"Particle: Halo Array");
       for(i=0;i<ptr[j].nHalos;i++)
 	{
+	  printf("free particle[%llu]\n",i);
 	  memmgr_free(ptr[j].mhalos[i].Particles,ptr[j].mhalos[i].npart*sizeof(particlelist_t),buff);
+	  printf("free proglist[%llu]\n",i);
 	  free(ptr[j].mhalos[i].proglist);
 	}
       sprintf(buff,"Halo Array");
