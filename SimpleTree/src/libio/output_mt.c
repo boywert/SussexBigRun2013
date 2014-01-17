c0fd37b07fea204cbe4db9d7402b549b8a5f6a17
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-01-17 10:09:01 +0000

    autoupdate

--- SimpleTree/src/libio/output_mt.c
+++ SimpleTree/src/libio/output_mt.c
@@ -43,8 +43,7 @@ void create_subfind_substruct(m_halo_wrapper_t* haloB)
   hid_t ihalo;
   hid_t upperhost,hosthalo,ref,cursub,curid;
   m_halo_wrapper_t temp;
-  temp = *haloB;
-  temp = maphalo_to_host_mt(temp);
+  *haloB = maphalo_to_host_mt(*haloB);
   printf("Generate Subfind structure\n");
 
   for(ihalo=0;ihalo<haloB->nHalos; ihalo++)
