231d12e5b4bc96368076aee19ac598b38fef4249
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-01-21 03:04:56 +0000

    autoupdate

--- SimpleTree/src/libio/output_mt.c
+++ SimpleTree/src/libio/output_mt.c
@@ -110,6 +110,7 @@ void complete_clgal_aux(hid_t hid, clgal_aux_data_wrapper_t **aux_data, char* ou
   localid = hid%(uint64_t)pow(10,10);
 
   /* make sure to read the current catalogue */
+  printf("hid:%llu s:%d d:%d id:%llu\n",hid,snapid,domainid,localid);
   internalaux_read(&(aux_data[snapid][domainid]),outputfolder);
 
   for(i=0;i<aux_data[snapid][domainid].lgal_aux_halos[localid].nprogs;i++)
