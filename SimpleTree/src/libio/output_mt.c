04100bed606efbfeede87b49c95f25652adb388c
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-01-22 12:18:00 +0000

    autoupdate

--- SimpleTree/src/libio/output_mt.c
+++ SimpleTree/src/libio/output_mt.c
@@ -203,9 +203,9 @@ void treecrawler(hid_t hid, clgal_aux_data_wrapper_t **aux_data, int treenr, ful
   /*   { */
   /*     printf("id: %llu duplicated : original %d\n",aux_data[snapid][domainid].lgal_aux_halos[localid].globalRefID,aux_data[snapid][domainid].lgal_aux_halos[localid].TreeNr); */
   /*   } */
-  if(hid == 35001830000001676)
+  if(aux_data[snapid][domainid].lgal_aux_halos[localid].TreeNr > -1)
     {
-      printf("35001830000001676 %d -> %d | < %llu\n",aux_data[snapid][domainid].lgal_aux_halos[localid].TreeNr,treenr,aux_data[snapid][domainid].lgal_aux_halos[localid].Descendant);
+      printf("%llu %d -> %d | < %llu\n",aux_data[snapid][domainid].lgal_aux_halos[localid].globalRefID,aux_data[snapid][domainid].lgal_aux_halos[localid].TreeNr,treenr,aux_data[snapid][domainid].lgal_aux_halos[localid].Descendant);
     }
   aux_data[snapid][domainid].lgal_aux_halos[localid].TreeNr = treenr;
   aux_data[snapid][domainid].lgal_aux_halos[localid].hidTree = nHalosinTree[treenr];
