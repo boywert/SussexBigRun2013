32f35912e2252baa0d2a6f45401b2e21cd217ff5
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-09 13:11:23 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/galaxy_read.py
+++ L-Galaxies_Sep12/awk/python/galaxy_read.py
@@ -1,2 +1,16 @@
 import LGalaxy
-file = ""
+import numpy
+folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
+file_prefix = "SA_z7.66"
+firstfile = 0
+lastfile = 0
+
+filename = folder+file_prefix+"_"+firstfile
+f = open(filename,"rb")
+dummy = numpy.fromfile(f,numpy.int32,1)
+nTrees = dummy[0]
+dummy = numpy.fromfile(f,numpy.int32,1)
+nHalos = dummy[0]
+nTreeHalos = numpy.fromfile(f,numpy.int32,nTrees)
+Galaxy = numpy.fromfile(f,LGalaxy.struct_dtype,nHalos)
+f.close()
