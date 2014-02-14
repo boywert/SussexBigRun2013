5f80bd6f6eb9884102fc7659902053f497529a07
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-14 10:54:24 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/galaxy_read.py
+++ L-Galaxies_Sep12/awk/python/galaxy_read.py
@@ -17,7 +17,7 @@ def readsnap(folder,file_prefix,firstfile,lastfile):
     nHalos = 0
     nTreeHalos = numpy.array([],dtype=numpy.int32)
     Galaxy = numpy.array([],dtype=LGalaxyStruct.struct_dtype)
-    output_Galaxy = numpy.array([],dtype=numpy.float32)
+    output_Galaxy = numpy.array([],dtype=numpy.dtype([("DiskMass",numpy.float32])))
     for ifile in range(firstfile,lastfile+1):
         print "File:",ifile
         filename = folder+file_prefix+"_"+"%d"%(ifile)
@@ -31,7 +31,7 @@ def readsnap(folder,file_prefix,firstfile,lastfile):
         addednTreeHalos = numpy.fromfile(f,numpy.int32,this_nTrees)
         nTreeHalos = numpy.append(nTreeHalos,addednTreeHalos)
         addedGalaxy = numpy.fromfile(f,LGalaxyStruct.struct_dtype,this_nHalos)
-        output_addedGalaxy = addedGalaxy[:]["DiskMass"]
+        output_addedGalaxy[:]["DiskMass"] = addedGalaxy[:]["DiskMass"]
         output_Galaxy = numpy.append(output_Galaxy,output_addedGalaxy)
         #Galaxy = numpy.append(Galaxy,addedGalaxy)
         #print nTrees,nHalos,len(Galaxy)
@@ -40,9 +40,9 @@ def readsnap(folder,file_prefix,firstfile,lastfile):
 
 
 (nTrees,nHalos,nTreeHalos,Galaxy) = readsnap(folder,file_prefix,firstfile,lastfile)
-StellarMass += numpy.sum(Galaxy[:]["BulgeMass"]) + numpy.sum(Galaxy[:]["DiskMass"])
-BlackHoleMass += numpy.sum(Galaxy[:]["BlackHoleMass"])
-Sfr += numpy.sum(Galaxy[:]["Sfr"])
-print StellarMass,BlackHoleMass,Sfr
-print LGalaxyStruct.struct_dtype
+StellarMass += numpy.sum(Galaxy[:]["DiskMass"])
+#BlackHoleMass += numpy.sum(Galaxy[:]["BlackHoleMass"])
+#Sfr += numpy.sum(Galaxy[:]["Sfr"])
+print StellarMass #,BlackHoleMass,Sfr
+#print LGalaxyStruct.struct_dtype
  
