b005ea07f04aa0ff50a2a0960b4faba9011d3f1d
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-14 09:18:27 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/galaxy_read.py
+++ L-Galaxies_Sep12/awk/python/galaxy_read.py
@@ -5,7 +5,7 @@ import numpy
 folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
 file_prefix = "SA_z7.66"
 firstfile = 0
-lastfile = 0
+lastfile = 215
 StellarMass = 0.0
 for ifile in range(firstfile,lastfile+1):
     filename = folder+file_prefix+"_"+"%d"%(ifile)
@@ -17,7 +17,7 @@ for ifile in range(firstfile,lastfile+1):
     nTreeHalos = numpy.fromfile(f,numpy.int32,nTrees)
     Galaxy = numpy.fromfile(f,LGalaxyStruct.struct_dtype,nHalos)
     f.close()
-    print numpy.sum(Galaxy[:]["BulgeMass"]),numpy.sum(Galaxy[:]["DiskMass"])
+    StellarMass += numpy.sum(Galaxy[:]["BulgeMass"]) + numpy.sum(Galaxy[:]["DiskMass"])
         
 
  
