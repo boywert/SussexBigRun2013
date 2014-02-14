8feb36287d289286b3455b6e870c47ad1a32ef3d
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-14 09:09:05 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/galaxy_read.py
+++ L-Galaxies_Sep12/awk/python/galaxy_read.py
@@ -6,25 +6,17 @@ folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/me
 file_prefix = "SA_z7.66"
 firstfile = 0
 lastfile = 0
+StellarMass = 0.0
+for ifile in range(firstfile,lastfile):
+    filename = folder+file_prefix+"_"+"%d"%(ifile)
+    f = open(filename,"rb")
+    dummy = numpy.fromfile(f,numpy.int32,1)
+    nTrees = dummy[0]
+    dummy = numpy.fromfile(f,numpy.int32,1)
+    nHalos = dummy[0]
+    nTreeHalos = numpy.fromfile(f,numpy.int32,nTrees)
+    Galaxy = numpy.fromfile(f,LGalaxyStruct.struct_dtype,nHalos)
+    f.close()
+    print Galaxy[:]["Type"]
+
  
-filename = folder+file_prefix+"_"+"%d"%(firstfile)
-f = open(filename,"rb")
-dummy = numpy.fromfile(f,numpy.int32,1)
-nTrees = dummy[0]
-dummy = numpy.fromfile(f,numpy.int32,1)
-nHalos = dummy[0]
-nTreeHalos = numpy.fromfile(f,numpy.int32,nTrees)
-Galaxy = numpy.fromfile(f,LGalaxyStruct.struct_dtype,nHalos)
-print LGalaxyStruct.struct_dtype
-f.close()
-
-properties = LGalaxyStruct.properties_used
-properties['DiskMass'] = True
-
-
-for el in LGalaxyStruct.properties_used.keys():
-    if(LGalaxyStruct.properties_used[el] is True):
-        print el, LGalaxyStruct.properties_used[el]
-        out = numpy.dtype([(el,LGalaxyStruct.struct_dtype[el])])
-        
-print out[0]
