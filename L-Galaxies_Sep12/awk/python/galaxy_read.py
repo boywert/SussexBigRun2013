86a5d5e0f5ce65299acef2ff2835d4f0b31c3477
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-20 20:23:26 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/galaxy_read.py
+++ L-Galaxies_Sep12/awk/python/galaxy_read.py
@@ -1,16 +1,6 @@
 import LGalaxyStruct
 import numpy
 
-
-folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
-snaplist_file = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/cubep3m_zlist_out"
-firstfile = 0
-lastfile = 0
-
-
-
-
-
 def readsnap(folder,file_prefix,firstfile,lastfile,filter_arr=LGalaxyStruct.properties_used):
     nTrees = 0
     nHalos = 0
@@ -41,26 +31,30 @@ def readsnap(folder,file_prefix,firstfile,lastfile,filter_arr=LGalaxyStruct.prop
        
       
         f.close()
-    return output_Galaxy
-    #return (StellarMass ,BlackHoleMass,Sfr)
+    return (nTree,nHalos,nTreeHalos,output_Galaxy)
 
-f = open(snaplist_file)
-lines = f.readlines()
-f.close()
-i=0
+def example():
 
-filter = LGalaxyStruct.properties_used
-filter['Mvir'] = False
-filter['Mag'] = False
-filter['MagDust'] = True
-for this_line in lines:
-    i+=1
-    if(i == 57):
-        #print "z",this_line.strip()
-        file_prefix = "SA_z"+this_line.strip()
-        gal = readsnap(folder,file_prefix,firstfile,lastfile,filter)
-        print gal['MagDust']
-        help(gal)
-        #print 1./(float(this_line.strip())+1.), StellarMass ,BlackHoleMass,Sfr
-#print LGalaxyStruct.struct_dtype
+    folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
+    snaplist_file = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/cubep3m_zlist_out"
+    firstfile = 0
+    lastfile = 215
+
+    f = open(snaplist_file)
+    lines = f.readlines()
+    f.close()
+    i=0
+    
+    filter = LGalaxyStruct.properties_used
+    filter['Mvir'] = False
+    filter['Mag'] = False
+    filter['MagDust'] = True
+    for this_line in lines:
+        i+=1
+        if(i == 57):
+            file_prefix = "SA_z"+this_line.strip()
+            gal = readsnap(folder,file_prefix,firstfile,lastfile,filter)
+            print gal['MagDust']
+            help(gal)
+      
  
