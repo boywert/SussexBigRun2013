25baba9a835fb074dbe29f641c2879c9e817d7ff
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-20 20:01:48 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/galaxy_read.py
+++ L-Galaxies_Sep12/awk/python/galaxy_read.py
@@ -33,12 +33,12 @@ def readsnap(folder,file_prefix,firstfile,lastfile,filter_arr=LGalaxyStruct.prop
         addednTreeHalos = numpy.fromfile(f,numpy.int32,this_nTrees)
         nTreeHalos = numpy.append(nTreeHalos,addednTreeHalos)
         this_addedGalaxy = numpy.fromfile(f,LGalaxyStruct.struct_dtype,this_nHalos)
-        addedGalaxy = numpy.array([],dtype=filter_dtype,this_nHalos)
+        #addedGalaxy = numpy.array([],dtype=filter_dtype,this_nHalos)
         for prop in LGalaxyStruct.struct_dtype.names:
             if(filter_arr[prop] is True):
                 print this_addedGalaxy[prop]
                 #addedGalaxy[prop] = this_addedGalaxy[prop]
-        output_Galaxy = numpy.append(output_Galaxy,addedGalaxy)
+        #output_Galaxy = numpy.append(output_Galaxy,addedGalaxy)
        
       
         f.close()
