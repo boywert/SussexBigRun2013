86e32f4be3ffd1ed9dd2cf998b6cb84cf509c89e
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-26 03:23:53 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/uv_luminosity.py
+++ L-Galaxies_Sep12/awk/python/uv_luminosity.py
@@ -2,7 +2,7 @@ import pylab
 import read_lgal
 import LGalaxyStruct
 
-def uv_l():
+def uv_l_z8():
     folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
     snaplist_file = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/cubep3m_zlist_out"
     firstfile = 0
@@ -15,10 +15,9 @@ def uv_l():
     filter['Mvir'] = False
     filter['Mag'] = False
     filter['MagDust'] = True
-    for this_line in lines:
-        i+=1
-        if(i == 57):
-            file_prefix = "SA_z"+this_line.strip()
-            (nTrees,nHalos,nTreeHalos,gal) = read_lgal.readsnap_lgal(folder,file_prefix,firstfile,lastfile,filter)
-            print gal['MagDust'][:,5]
-            #help(gal)
+
+    file_prefix = "SA_z8.06"    
+    (nTrees,nHalos,nTreeHalos,gal) = read_lgal.readsnap_lgal(folder,file_prefix,firstfile,lastfile,filter)
+    lgal_hist = pylab.histogram(gal['MagDust'][:,5])
+    
+    lgal_hist
