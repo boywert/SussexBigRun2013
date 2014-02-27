42dc71cd2cb0e595ca03c02a52aa9a5ad422ac5a
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-27 03:28:14 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/uv_luminosity.py
+++ L-Galaxies_Sep12/awk/python/uv_luminosity.py
@@ -46,8 +46,8 @@ def uv_l_z8():
     bouwens2011_x = bouwens2011[:,0]-5.*numpy.log10(hubble_h)
     bouwens2011_y = (10.**bouwens2011[:,1])/hubble_h**3.
 
-    bouwens2011_errorup = 10.**bouwens2011[:,5]/hubble_h**3.
-    bouwens2011_errordown = 10.**bouwens2011[:,4]/hubble_h**3.
+    bouwens2011_errorup = (10.**(bouwens2011[:,1] + bouwens2011[:,5]) - 10.**bouwens2011[:,1])/hubble_h**3.
+    bouwens2011_errordown = (10.**bouwens2011[:,1] - 10.**(bouwens2011[:,1] - bouwens2011[:,4]))/hubble_h**3.
     ax.errorbar(bouwens2011_x,bouwens2011_y,yerr=[bouwens2011_errorup,bouwens2011_errordown], fmt='o')
     ax.set_yscale("log")
     print nGals, len(gal)
