cd531918fc8b9c4ef1be780e64e466e876db1392
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-02-27 03:43:50 +0000

    autoupdate

--- L-Galaxies_Sep12/awk/python/uv_luminosity.py
+++ L-Galaxies_Sep12/awk/python/uv_luminosity.py
@@ -9,7 +9,8 @@ def uv_l_z8():
     min_mag = -23.
     nbins=14
     hubble_h = 0.7
-    folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
+    model2_folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs/"
+    nore_folder = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/outputs_nore/"
     snaplist_file = "/mnt/lustre/scratch/cs390/AHF_halos/cubepm_131212_6_1728_47Mpc_ext2/mergertrees/cubep3m_zlist_out"
     observe_folder="/mnt/lustre/scratch/cs390/codes/cubepm_131212_6_1728_47Mpc_ext2/observed_UVL/"
     firstfile = 0
@@ -24,7 +25,7 @@ def uv_l_z8():
     filter['MagDust'] = True
 
     file_prefix = "SA_z8.06"    
-    (nTrees,nGals,nTreeGals,gal) = read_lgal.readsnap_lgal(folder,file_prefix,firstfile,lastfile,filter)
+    (nTrees,nGals,nTreeGals,gal) = read_lgal.readsnap_lgal(model2_folder,file_prefix,firstfile,lastfile,filter)
     lgal_hist_metal = pylab.histogram(gal['MagDust'][:,5],bins=nbins,range=(min_mag,max_mag))
     
     lgal_hist_metal_y = lgal_hist_metal[0]
@@ -36,11 +37,28 @@ def uv_l_z8():
     lgal_hist_metal_y = lgal_hist_metal_y / boxsize**3. / (lgal_hist_metal_x[1]-lgal_hist_metal_x[0])
     lgal_hist_total =  pylab.histogram(gal['Mag'][:,5],bins=nbins,range=(min_mag,max_mag))
     lgal_hist_total_y = lgal_hist_total[0]/ boxsize**3. / (lgal_hist_metal_x[1]-lgal_hist_metal_x[0])
+
+    (nTrees,nGals,nTreeGals,gal2) = read_lgal.readsnap_lgal(nore_folder,file_prefix,firstfile,lastfile,filter)
+    lgal_hist_metal_nr = pylab.histogram(gal2['MagDust'][:,5],bins=nbins,range=(min_mag,max_mag))
+    
+    lgal_hist_metal_nr_y = lgal_hist_metal_nr[0]
+    lgal_hist_metal_nr_x = []
+    for i in range(len(lgal_hist_metal_nr_y)):
+        lgal_hist_metal_nr_x.append((lgal_hist_metal_nr[1][i]+lgal_hist_metal_nr[1][i+1])/2.)
+    
+    lgal_hist_metal_nr_x = lgal_hist_metal_nr_x - 5.*numpy.log10(hubble_h)
+    lgal_hist_metal_nr_y = lgal_hist_metal_nr_y / boxsize**3. / (lgal_hist_metal_nr_x[1]-lgal_hist_metal_nr_x[0])
+    lgal_hist_total_nr =  pylab.histogram(gal2['Mag'][:,5],bins=nbins,range=(min_mag,max_mag))
+    lgal_hist_total_nr_y = lgal_hist_total_nr[0]/ boxsize**3. / (lgal_hist_metal_nr_x[1]-lgal_hist_metal_nr_x[0])
+
+
     pylab.rc('text', usetex=True)
     fig = pylab.figure()
     ax = fig.add_subplot(111)
-    ax.plot(lgal_hist_metal_x,lgal_hist_metal_y)
-    ax.plot(lgal_hist_metal_x,lgal_hist_total_y)
+    ax.plot(lgal_hist_metal_x,lgal_hist_metal_y,'r-')
+    ax.plot(lgal_hist_metal_x,lgal_hist_total_y,'b-')
+    ax.plot(lgal_hist_metal_nr_x,lgal_hist_metal_nr_y,'r--')
+    ax.plot(lgal_hist_metal_nr_x,lgal_hist_total_nr_y,'b--')
     bouwens2011_file = observe_folder+"bouwens2011_z8.txt"
     bouwens2011 = numpy.loadtxt(bouwens2011_file)
     bouwens2011_x = bouwens2011[:,0]-5.*numpy.log10(hubble_h)
