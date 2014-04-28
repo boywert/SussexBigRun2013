4d95dbc285d1c7df694d9f2cb0b10cc4276758e2
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-04-28 11:20:15 +0100

    autoupdate

--- L-Galaxies_development/python/nifty_convert.py
+++ L-Galaxies_development/python/nifty_convert.py
@@ -3,7 +3,7 @@ import numpy
 import read_lgal
 import LGalaxyStruct
 
-def convert_nifty():
+def read_galaxies():
     boxsize = 62.5*1000. #kpc/h
     Mpc2kpc = 1000.0
     Gadget2Msun = 1.e10
@@ -41,23 +41,11 @@ def convert_nifty():
 
     file_prefix = "SA_"    
     (nGals,gal) = read_lgal.read_lgaltree(folder,file_prefix,firstfile,lastfile,filter)
+    halomap = {}
     for idgal in range(nGals):
         galaxy = gal[idgal]
-        haloid = galaxy["HaloID"]
-        Mstar = galaxy['DiskMass']+galaxy['BulgeMass']
-        X = galaxy["Pos"][0]*Mpc2kpc
-        Y = galaxy["Pos"][1]*Mpc2kpc
-        Z = galaxy["Pos"][2]*Mpc2kpc
-        VX = galaxy["Vel"][0]
-        VY = galaxy["Vel"][1]
-        VZ = galaxy["Vel"][2]
-        Mcold = galaxy["ColdGas"]*Gadget2Msun
-        Mhot = galaxy["HotGas"]*Gadget2Msun
-        Mbh = galaxy["BlackHoleMass"]*Gadget2Msun
-        Z_gas = (galaxy["MetalsColdGas"] + galaxy["MetalsHotGas"])*Gadget2Msun
-        Z_stars = (galaxy["MetalsBulgeMass"] + galaxy["MetalsDiskMass"])*Gadget2Msun
-        T_stars = galaxy["MassWeightAge"]
         Type = galaxy["Type"]
+        
 
         if(Type != 2):
             lasthaloid = haloid
@@ -66,7 +54,74 @@ def convert_nifty():
             while (gal[last_main_prog]["Type"] == 2):
                 last_main_prog = last_main_prog+1
             lasthaloid = gal[last_main_prog]["HaloID"]
+        galaxy["LastHalo"] = lasthaloid
 
-        print haloid,lasthaloid
+        if haloid in halomap:
+            halomap[haloid].append(idgal)
+        else:
+            halomap[haloid] = []
+            halomap[haloid].append(idgal)
 
-convert_nifty()
+    return (nGals,gal,halomap)
+
+
+(nGals,gal,halomap) = convert_nifty()
+
+
+AHFdir = "/mnt/lustre/scratch/cs390/nIFTy/62.5_dm/AHF/"
+AHFprefix = "62.5_dm"
+SUSSINGtree = "/mnt/lustre/scratch/cs390/nIFTy/62.5_dm/MergerTree+AHF.txt"
+SNAPfile = "/mnt/lustre/scratch/cs390/nIFTy/62.5_dm/snapidzred.txt"
+FileOut = "/mnt/lustre/scratch/cs390/nIFTy/62.5_dm/treedata/trees_061.0"
+FileOut2 = "/mnt/lustre/scratch/cs390/nIFTy/62.5_dm/treedata/tree_dbids_061.0"
+timesnap = numpy.loadtxt(SNAPfile)
+for time in timesnap:
+    zstring = "%.3f" % (time[2])
+        #print zstring[len(zstring)-1]
+    filename = "%s/%s_%03d.z%s.AHF_halos" % (AHFdir, AHFprefix, time[0], zstring)
+        #print "checking "+filename
+    if os.path.isfile(filename) == False:
+        zstring = "%.3f" % (time[2]+0.00001)
+            #print zstring[len(zstring)-1]
+        filename = "%s/%s_%03d.z%s.AHF_halos" % (AHFdir, AHFprefix, time[0], zstring)
+    if os.path.isfile(filename) == False:
+        zstring = "%.3f" % (time[2]-0.00001)
+            #print "checking "+filename
+            #print zstring[len(zstring)-1]
+        filename = "%s/%s_%03d.z%s.AHF_halos" % (AHFdir, AHFprefix, time[0], zstring)
+    if os.path.isfile(filename) == False:
+        print "checking error"+filename
+        exit()
+    print "Reading "+filename
+    stat = os.stat(filename)
+        #print stat.st_size
+    if(stat.st_size > 384):
+        data = numpy.loadtxt(filename)
+        for halo in data:
+            hid = halo[0]
+            if hid in halomap:
+                print hid, len(halomap[hid])
+                for galid in halomap[hid]:
+                    galaxy = gal[galid]
+                    haloid = galaxy["HaloID"]
+                    Mstar = galaxy['DiskMass']+galaxy['BulgeMass']
+                    X = galaxy["Pos"][0]*Mpc2kpc
+                    Y = galaxy["Pos"][1]*Mpc2kpc
+                    Z = galaxy["Pos"][2]*Mpc2kpc
+                    VX = galaxy["Vel"][0]
+                    VY = galaxy["Vel"][1]
+                    VZ = galaxy["Vel"][2]
+                    Mcold = galaxy["ColdGas"]*Gadget2Msun
+                    Mhot = galaxy["HotGas"]*Gadget2Msun
+                    Mbh = galaxy["BlackHoleMass"]*Gadget2Msun
+                    Z_gas = (galaxy["MetalsColdGas"] + galaxy["MetalsHotGas"])*Gadget2Msun
+                    Z_stars = (galaxy["MetalsBulgeMass"] + galaxy["MetalsDiskMass"])*Gadget2Msun
+                    T_stars = galaxy["MassWeightAge"]
+                    if(galaxy["Type"] == 2):
+                        orphan_flag = 1
+                    else:
+                        orphan_flag = 0
+                    print galaxy["LastHalo"],orphan_flag
+            else:
+                print hid, 0
+
