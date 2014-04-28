f15e51e198779bfdc86b286aea63d037d053aed3
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-04-28 06:59:14 +0100

    autoupdate

--- L-Galaxies_development/python/nifty_convert.py
+++ L-Galaxies_development/python/nifty_convert.py
@@ -4,10 +4,12 @@ import read_lgal
 import LGalaxyStruct
 
 def convert_nifty():
-    boxsize = 47.0
+    boxsize = 62.5*1000. #kpc/h
+    Mpc2kpc = 1000.0
+    Gadget2Msun = 1.e10
 
     folder = "/mnt/lustre/scratch/cs390/nIFTy/62.5_dm/outputs/"
-
+    
 
     firstfile = 0
     lastfile = 0
@@ -34,27 +36,35 @@ def convert_nifty():
     filter['MetalsBulgeMass'] = True
     filter['MetalsDiskMass'] = True
     filter['MassWeightAge'] = True
+    filter["FirstProgGal"] = True
 
-    print filter
     file_prefix = "SA_"    
     (nGals,gal) = read_lgal.read_lgaltree(folder,file_prefix,firstfile,lastfile,filter)
     for galaxy in gal:
         haloid = galaxy["HaloID"]
         Mstar = galaxy['DiskMass']+galaxy['BulgeMass']
-        X = galaxy["Pos"][0]
-        Y = galaxy["Pos"][1]
-        Z = galaxy["Pos"][2]
+        X = galaxy["Pos"][0]*Mpc2kpc
+        Y = galaxy["Pos"][1]*Mpc2kpc
+        Z = galaxy["Pos"][2]*Mpc2kpc
         VX = galaxy["Vel"][0]
         VY = galaxy["Vel"][1]
         VZ = galaxy["Vel"][2]
-        Mcold = galaxy["ColdGas"]
-        Mhot = galaxy["HotGas"]
-        Mbh = galaxy["BlackHoleMass"]
-        Z_gas = galaxy["MetalsColdGas"] + galaxy["MetalsHotGas"]
-        Z_stars = galaxy["MetalsBulgeMass"] + galaxy["MetalsDiskMass"]
+        Mcold = galaxy["ColdGas"]*Gadget2Msun
+        Mhot = galaxy["HotGas"]*Gadget2Msun
+        Mbh = galaxy["BlackHoleMass"]*Gadget2Msun
+        Z_gas = (galaxy["MetalsColdGas"] + galaxy["MetalsHotGas"])*Gadget2Msun
+        Z_stars = (galaxy["MetalsBulgeMass"] + galaxy["MetalsDiskMass"])*Gadget2Msun
         T_stars = galaxy["MassWeightAge"]
         Type = galaxy["Type"]
-        if(haloid == 60000000000176):
-            print haloid,Type, Mstar, X, Y, Z, VX, VY, VZ, Mcold, Mhot, Mbh, Z_gas, Z_stars, T_stars
+        ProgGal = galaxy["FirstProgGal"]
+        if(Type != 2):
+            lasthaloid = haloid
+        else:
+            last_main_prog = ProgGal
+            while (gal[last_main_prog]["Type"] == 2):
+                last_main_prog = gal[last_main_prog]["FirstProgGal"]
+            lasthaloid = gal[lastfile]["HaloID"]
+        # if(haloid == 60000000000176):
+        #     print haloid,Type, Mstar, X, Y, Z, VX, VY, VZ, Mcold, Mhot, Mbh, Z_gas, Z_stars, T_stars
 
 convert_nifty()
