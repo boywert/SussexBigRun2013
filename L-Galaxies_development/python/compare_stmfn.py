f36a752625f685c156c7099752482bad155faf6e
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-05 02:52:20 +0100

    autoupdate

--- L-Galaxies_development/python/compare_stmfn.py
+++ L-Galaxies_development/python/compare_stmfn.py
@@ -2,7 +2,7 @@ from stellar_mass_fn import *
 import pylab
 
 okamoto_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/","SA_z6.00",0,127,1.e3,1.e12,50)
-
+noreionization_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","SA_z6.00",0,127,1.e3,1.e12,50)
 print okamoto_model
 pylab.rc('text', usetex=True)
 fig = pylab.figure()
@@ -10,7 +10,7 @@ ax = fig.add_subplot(111)
 
 
 ax.plot(okamoto_model[0],okamoto_model[1],'r-',label="Okamoto et al. (2008)")
-
+ax.plot(noreionization_model[0],noreionization_model[1],'b-',label="No Reionization")
 
 
 ax.set_yscale("log")
