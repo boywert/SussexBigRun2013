7317598162e22eb7dfaec9d1bae511a778fa1d5e
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-05 01:38:23 +0100

    autoupdate

--- L-Galaxies_development/python/compare_stmfn.py
+++ L-Galaxies_development/python/compare_stmfn.py
@@ -1,4 +1,4 @@
-from steller_mass_fn import *
+from stellar_mass_fn import *
 
-okamoto_model = steller_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/","SA_z6.00",0,127,1.e6,1.e12,20)
+okamoto_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/","SA_z6.00",0,127,1.e6,1.e12,20)
 print okamoto_model
