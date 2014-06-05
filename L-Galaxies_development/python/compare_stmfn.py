8b7d2493c30a75d50755cd7fc3fc3da6b013a438
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-05 12:54:20 +0100

    autoupdate

--- L-Galaxies_development/python/compare_stmfn.py
+++ L-Galaxies_development/python/compare_stmfn.py
@@ -4,6 +4,7 @@ import pylab
 okamoto_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/","SA_z6.00",0,127,1.e3,1.e12,50)
 noreionization_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","SA_z6.00",0,127,1.e3,1.e12,50)
 fullreionization_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/full_reionization/","SA_z6.00",0,127,1.e3,1.e12,50)
+patchyreionization_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization/","SA_z6.00",0,127,1.e3,1.e12,50)
 print okamoto_model
 pylab.rc('text', usetex=True)
 fig = pylab.figure()
@@ -11,8 +12,9 @@ ax = fig.add_subplot(111)
 
 
 ax.plot(okamoto_model[0],okamoto_model[1],'r-',label="Okamoto et al. (2008)")
-ax.plot(noreionization_model[0],noreionization_model[1],'b-',label="No Reionization")
+ax.plot(noreionization_model[0],noreionization_model[1],'b--',label="No Reionization")
 ax.plot(fullreionization_model[0],fullreionization_model[1],'g-',label="Full Reionization")
+ax.plot(patchyreionization_model[0],patchyreionization_model[1],'m--',label="Patchy Reionization")
 
 
 ax.set_yscale("log")
