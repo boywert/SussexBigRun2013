0076e7f2d0e2341e8d68fba3739130893842e6de
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-10 16:32:58 +0100

    autoupdate

--- L-Galaxies_development/code/init.c
+++ L-Galaxies_development/code/init.c
@@ -485,7 +485,8 @@ void set_units(void)
   UnitTime_in_Megayears = UnitTime_in_s / SEC_PER_MEGAYEAR;
   UnitTime_in_years = 1e6*UnitTime_in_Megayears;
 
-
+  printf("G=%f,H=%f\n",GRAVITY,HUBBLE);
+  exit(0);
   //gravity in internal units
   G = GRAVITY / pow3(UnitLength_in_cm) * UnitMass_in_g * pow2(UnitTime_in_s);//43.00708
 
--- L-Galaxies_development/code/recipe_infall.c
+++ L-Galaxies_development/code/recipe_infall.c
@@ -85,7 +85,7 @@ double infall_recipe(int centralgal, int ngal, double Zcurr)
 	  Tk = 0.5*mu*m_p/BOLTZMANN*(double)pow(0.5*200.*Omega,1./3.)
 	    *(1.+ZZ[Gal[centralgal].SnapNum])
 	    *(double)pow(GRAVITY*HUBBLE*Gal[centralgal].CentralMvir*UnitMass_in_g,2./3.);
-	  printf("T=%lf,Gravity=%f,Mvir=%f\n",Tk,GRAVITY,Gal[centralgal].CentralMvir);
+	  // printf("H=%f,Gravity=%f\n",Tk,GRAVITY,Gal[centralgal].CentralMvir);
 	  if(Tk < 10000.)
 	    reionization_modifier = 0.;
 	  else
