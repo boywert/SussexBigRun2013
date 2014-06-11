69b6f40891d81acf02a8c23e7a8efd8aea557519
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-11 18:48:02 +0100

    autoupdate

--- L-Galaxies_development/python/compare_stmfn.py
+++ L-Galaxies_development/python/compare_stmfn.py
@@ -24,30 +24,49 @@ cold_patchyreionization_model_II = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47
 
 
 pylab.rc('text', usetex=True)
+
 fig = pylab.figure()
 ax = fig.add_subplot(111)
-
-
 ax.plot(star_okamoto_model[0],star_okamoto_model[1],'r--',label="Okamoto et al. (2008)")
 ax.plot(star_noreionization_model[0],star_noreionization_model[1],'b--',label="No Reionization")
 ax.plot(star_patchyreionization_model[0],star_patchyreionization_model[1],'m--',label="Patchy Reionization")
 ax.plot(star_patchyreionization_model_II[0],star_patchyreionization_model_II[1],'g--',label="Patchy Reionization II")
-
-ax.plot(hot_okamoto_model[0],hot_okamoto_model[1],'r--')
-ax.plot(hot_noreionization_model[0],hot_noreionization_model[1],'b--')
-ax.plot(hot_patchyreionization_model[0],hot_patchyreionization_model[1],'m--')
-ax.plot(hot_patchyreionization_model_II[0],hot_patchyreionization_model_II[1],'g--')
-
-ax.plot(cold_okamoto_model[0],cold_okamoto_model[1],'r--')
-ax.plot(cold_noreionization_model[0],cold_noreionization_model[1],'b--')
-ax.plot(cold_patchyreionization_model[0],cold_patchyreionization_model[1],'m--')
-ax.plot(cold_patchyreionization_model_II[0],cold_patchyreionization_model_II[1],'g--')
-
 ax.set_yscale("log")
 leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
 leg.get_frame().set_linewidth(0)
 
 ax.set_xlabel(r"$\log(M/M_\odot h)$")
 ax.set_ylabel(r"$N$")
-fig.suptitle("Stellar Mass Function z = 6")
-pylab.savefig('reion_model_8.pdf',bbox_inches='tight')
+fig.suptitle("Stellar Mass Function z = 6 file "+str(ff)+"-"+str(lf))
+pylab.show()
+pylab.savefig('reion_star_'+str(ff)+''+str(lf)+'.pdf',bbox_inches='tight')
+
+fig = pylab.figure()
+ax = fig.add_subplot(111)
+ax.plot(hot_okamoto_model[0],hot_okamoto_model[1],'r--')
+ax.plot(hot_noreionization_model[0],hot_noreionization_model[1],'b--')
+ax.plot(hot_patchyreionization_model[0],hot_patchyreionization_model[1],'m--')
+ax.plot(hot_patchyreionization_model_II[0],hot_patchyreionization_model_II[1],'g--')
+ax.set_yscale("log")
+leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
+leg.get_frame().set_linewidth(0)
+
+ax.set_xlabel(r"$\log(M/M_\odot h)$")
+ax.set_ylabel(r"$N$")
+fig.suptitle("Hot Gass Mass Function z = 6 file "+str(ff)+"-"+str(lf))
+pylab.show()
+pylab.savefig('reion_hotgas_'+str(ff)+''+str(lf)+'.pdf',bbox_inches='tight')
+
+
+fig = pylab.figure()
+ax = fig.add_subplot(111)
+ax.plot(cold_okamoto_model[0],cold_okamoto_model[1],'r--')
+ax.plot(cold_noreionization_model[0],cold_noreionization_model[1],'b--')
+ax.plot(cold_patchyreionization_model[0],cold_patchyreionization_model[1],'m--')
+ax.plot(cold_patchyreionization_model_II[0],cold_patchyreionization_model_II[1],'g--')
+
+ax.set_xlabel(r"$\log(M/M_\odot h)$")
+ax.set_ylabel(r"$N$")
+fig.suptitle("Cold Gass Mass Function z = 6 file "+str(ff)+"-"+str(lf))
+pylab.show()
+pylab.savefig('reion_coldgas_'+str(ff)+''+str(lf)+'.pdf',bbox_inches='tight')
