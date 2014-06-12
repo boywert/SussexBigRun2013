f8f94e7fc66c1730e29b82b3663ce4f2469695b8
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-12 17:42:47 +0100

    autoupdate

--- L-Galaxies_development/python/compare_z6.py
+++ L-Galaxies_development/python/compare_z6.py
@@ -8,35 +8,35 @@ star_okamoto_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/ok
 star_noreionization_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/",prefix,ff,lf,1.e3,1.e12,50)
 star_patchyreionization_model = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_I/",prefix,ff,lf,1.e3,1.e12,50)
 star_patchyreionization_model_II = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_II/",prefix,ff,lf,1.e3,1.e12,50)
-star_patchyreionization_model_III = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf,1.e3,1.e12,50)
+#star_patchyreionization_model_III = stellar_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf,1.e3,1.e12,50)
 
 
 hot_okamoto_model = hotgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/",prefix,ff,lf,1.e3,1.e12,50)
 hot_noreionization_model = hotgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/",prefix,ff,lf,1.e3,1.e12,50)
 hot_patchyreionization_model = hotgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_I/",prefix,ff,lf,1.e3,1.e12,50)
 hot_patchyreionization_model_II = hotgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_II/",prefix,ff,lf,1.e3,1.e12,50)
-hot_patchyreionization_model_III = hotgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf,1.e3,1.e12,50)
+#hot_patchyreionization_model_III = hotgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf,1.e3,1.e12,50)
 
 
 bh_okamoto_model = bh_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/",prefix,ff,lf)
 bh_noreionization_model = bh_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/",prefix,ff,lf)
 bh_patchyreionization_model = bh_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_I/",prefix,ff,lf)
 bh_patchyreionization_model_II = bh_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_II/",prefix,ff,lf)
-bh_patchyreionization_model_III = bh_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf)
+#bh_patchyreionization_model_III = bh_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf)
 
 
 cold_okamoto_model = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/",prefix,ff,lf,1.e3,1.e12,50)
 cold_noreionization_model = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/",prefix,ff,lf,1.e3,1.e12,50)
 cold_patchyreionization_model = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_I/",prefix,ff,lf,1.e3,1.e12,50)
 cold_patchyreionization_model_II = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_II/",prefix,ff,lf,1.e3,1.e12,50)
-cold_patchyreionization_model_III = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf,1.e3,1.e12,50)
+#cold_patchyreionization_model_III = coldgas_mass_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf,1.e3,1.e12,50)
 
 
 sfr_okamoto_model = sfr_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/",prefix,ff,lf)
 sfr_noreionization_model = sfr_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/",prefix,ff,lf)
 sfr_patchyreionization_model = sfr_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_I/",prefix,ff,lf)
 sfr_patchyreionization_model_II = sfr_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_II/",prefix,ff,lf)
-sfr_patchyreionization_model_III = sfr_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf)
+#sfr_patchyreionization_model_III = sfr_fn("/mnt/lustre/scratch/cs390/47Mpc/outputs/patchy_reionization_III/",prefix,ff,lf)
 
 
 pylab.rc('text', usetex=True)
@@ -47,7 +47,7 @@ ax.plot(star_okamoto_model[0],star_okamoto_model[1],'r--',label="Okamoto et al. 
 ax.plot(star_noreionization_model[0],star_noreionization_model[1],'b--',label="No Reionization")
 ax.plot(star_patchyreionization_model[0],star_patchyreionization_model[1],'m--',label="Patchy Reionization")
 ax.plot(star_patchyreionization_model_II[0],star_patchyreionization_model_II[1],'g--',label="Patchy Reionization II")
-ax.plot(star_patchyreionization_model_III[0],star_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
+#ax.plot(star_patchyreionization_model_III[0],star_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
 ax.set_yscale("log")
 leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
 leg.get_frame().set_linewidth(0)
@@ -82,7 +82,7 @@ ax.plot(cold_okamoto_model[0],cold_okamoto_model[1],'r--',label="Okamoto et al. 
 ax.plot(cold_noreionization_model[0],cold_noreionization_model[1],'b--',label="No Reionization")
 ax.plot(cold_patchyreionization_model[0],cold_patchyreionization_model[1],'m--',label="Patchy Reionization")
 ax.plot(cold_patchyreionization_model_II[0],cold_patchyreionization_model_II[1],'g--',label="Patchy Reionization II")
-ax.plot(cold_patchyreionization_model_III[0],cold_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
+#ax.plot(cold_patchyreionization_model_III[0],cold_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
 ax.set_yscale("log")
 leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
 leg.get_frame().set_linewidth(0)
@@ -99,7 +99,7 @@ ax.plot(bh_okamoto_model[0],bh_okamoto_model[1],'r--',label="Okamoto et al. (200
 ax.plot(bh_noreionization_model[0],bh_noreionization_model[1],'b--',label="No Reionization")
 ax.plot(bh_patchyreionization_model[0],bh_patchyreionization_model[1],'m--',label="Patchy Reionization")
 ax.plot(bh_patchyreionization_model_II[0],bh_patchyreionization_model_II[1],'g--',label="Patchy Reionization II")
-ax.plot(bh_patchyreionization_model_III[0],bh_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
+#ax.plot(bh_patchyreionization_model_III[0],bh_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
 #ax.set_yscale("log")
 leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
 leg.get_frame().set_linewidth(0)
@@ -116,7 +116,7 @@ ax.plot(sfr_okamoto_model[0],sfr_okamoto_model[1],'r--',label="Okamoto et al. (2
 ax.plot(sfr_noreionization_model[0],sfr_noreionization_model[1],'b--',label="No Reionization")
 ax.plot(sfr_patchyreionization_model[0],sfr_patchyreionization_model[1],'m--',label="Patchy Reionization")
 ax.plot(sfr_patchyreionization_model_II[0],sfr_patchyreionization_model_II[1],'g--',label="Patchy Reionization II")
-ax.plot(sfr_patchyreionization_model_III[0],sfr_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
+#ax.plot(sfr_patchyreionization_model_III[0],sfr_patchyreionization_model_III[1],'k--',label="Patchy Reionization III")
 ax.set_yscale("log")
 ax.set_xscale("log")
 leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
