e4e5d70178dc2c7145d4e2e7a4f98296ceb69ba9
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-20 13:39:35 +0100

    autoupdate

--- L-Galaxies_development/code/read_xfrac.c
+++ L-Galaxies_development/code/read_xfrac.c
@@ -112,7 +112,7 @@ int read_xfrac(int snapnr, double* xfrac)
     }
   for(i=0;i<XfracMesh[0]*XfracMesh[1]*XfracMesh[2];i++)
     {
-      printf("Task:%d, %lf\n",i,xfrac[i]);
+      printf("Task:%d, %lf\n",ThisTask,xfrac[i]);
       MPI_Barrier(MPI_COMM_WORLD);
     }
 #endif
