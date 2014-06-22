9fff5517a9374f28eed4e56fc4a87242c1d9e1d5
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-22 10:48:18 +0100

    autoupdate

--- L-Galaxies_development/python/compare_z6.py
+++ L-Galaxies_development/python/compare_z6.py
@@ -119,7 +119,7 @@ ax.scatter(gal["okamoto"]["BulgeMass"]+gal["okamoto"]["DiskMass"],gal["okamoto"]
 #         print gal["okamoto"]["BulgeMass"][i]+gal["okamoto"]["DiskMass"][i],gal["okamoto"]["DiskMass"][i]
 
 # ax.set_yscale("log")
-# ax.set_xscale("log")
+ax.set_xscale("log")
 
 leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
 leg.get_frame().set_linewidth(0)
