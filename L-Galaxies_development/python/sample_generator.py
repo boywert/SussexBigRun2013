395d50bba3596c77c4faabdf78ecfcf4b1cdedfa
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-25 15:14:02 +0100

    autoupdate

--- L-Galaxies_development/python/sample_generator.py
+++ L-Galaxies_development/python/sample_generator.py
@@ -9,14 +9,20 @@ lastsnap = 61
 (nHalos,nTrees,ngalstree,output_trees) = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
 db = sqlite3.connect(':memory:')
 cursor = db.cursor()
-cursor.execute('''CREATE TABLE tree (curhalonr INTEGER,filenr INTEGER,treenr INTEGER,halonr INTEGER, mass REAL)''')
+cursor.execute('''CREATE TABLE tree (
+curhalonr INTEGER,
+filenr INTEGER,
+treenr INTEGER,
+halonr INTEGER, 
+mass REAL)
+''')
 
 count_halo = 0
 for j in range(nTrees):
     nh = ngalstree[j]
     for i in range(nh):
         if(output_trees[count_halo]['FirstHaloInFOFgroup'] == i):
-            cursor.execute("INSERT INTO tree VALUES (?,?,?,?,?)",(count_halo,output_trees[count_halo]['FileNr'],nh,j,i,output_trees[count_halo]['M_Crit200']))
+            cursor.execute("INSERT INTO tree VALUES (?,?,?,?,?)",(count_halo,output_trees[count_halo]['FileNr'],j,i,output_trees[count_halo]['M_Crit200']))
         count_halo += 1
 
 
