a3d31d49010893e370f64b3e24980a7f5e433c72
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-26 00:47:43 +0100

    autoupdate

--- L-Galaxies_development/python/sample_generator.py
+++ L-Galaxies_development/python/sample_generator.py
@@ -22,7 +22,10 @@ for j in range(nTrees):
     nh = ngalstree[j]
     for i in range(nh):
         if(output_trees[count_halo]['FirstHaloInFOFgroup'] == i):
-            cursor.execute("INSERT INTO tree(curhalonr,filenr,treenr,halonr,mass) VALUES (?,?,?,?,?)",(count_halo,output_trees[count_halo]['FileNr'],j,i,output_trees[count_halo]['M_Crit200']))
+            cursor.execute("INSERT INTO tree(curhalonr,filenr,treenr,halonr,mass) 
+VALUES (?,?,?,?,?)",(1,2,3,4,2.))
+
+#(count_halo,output_trees[count_halo]['FileNr'],j,i,output_trees[count_halo]['M_Crit200']))
             print output_trees[count_halo]
         count_halo += 1
 
