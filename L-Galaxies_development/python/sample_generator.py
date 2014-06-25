47bee72df3e500314f112c3122f47676be47ec28
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-25 11:01:20 +0100

    autoupdate

--- L-Galaxies_development/python/sample_generator.py
+++ L-Galaxies_development/python/sample_generator.py
@@ -7,7 +7,7 @@ firstfile = 0
 lastfile = 0
 lastsnap = 61
 (nHalos,nTrees,ngalstree,output_trees) = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
-print output_trees
+print nHalos,nTrees,ngalstree,output_trees
 db = sqlite3.connect(':memory:')
 
 db.close()
