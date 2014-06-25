f377e66c7aa59ce18d64c2a052e243d2a43770f2
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-25 14:57:38 +0100

    autoupdate

--- L-Galaxies_development/python/sample_generator.py
+++ L-Galaxies_development/python/sample_generator.py
@@ -9,9 +9,8 @@ lastsnap = 61
 (nHalos,nTrees,ngalstree,output_trees) = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
 db = sqlite3.connect(':memory:')
 cursor = db.cursor()
-# cursor.execute('''CREATE TABLE tree(index  INTEGER,filenr INTEGER,treenr INTEGER,halonr INTEGER, mass REAL)''')
-cursor.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+cursor.execute('''CREATE TABLE tree (index  INTEGER,filenr INTEGER,treenr INTEGER,halonr INTEGER, mass REAL)''')
+
 count_halo = 0
 for nh in ngalstree:
     for i in range(nh):
