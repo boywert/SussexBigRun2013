c862ec4596ba017ec99dd8ddb27ab328b53cd142
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-25 11:54:13 +0100

    autoupdate

--- L-Galaxies_development/python/sample_generator.py
+++ L-Galaxies_development/python/sample_generator.py
@@ -7,7 +7,14 @@ firstfile = 0
 lastfile = 0
 lastsnap = 61
 (nHalos,nTrees,ngalstree,output_trees) = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
-print nHalos,nTrees,ngalstree,output_trees
 db = sqlite3.connect(':memory:')
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE tree(index  INTEGER,
+                      filenr INTEGER, 
+                      treenr INTEGER,
+                      halonr INTEGER, 
+                      mass REAL)
+''')
 
 db.close()
