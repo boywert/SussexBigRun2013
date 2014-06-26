a7587b3bb830ddc4c2ab3e71baa4d2994b55ec89
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-26 01:22:27 +0100

    autoupdate

--- L-Galaxies_development/python/sample_generator.py
+++ L-Galaxies_development/python/sample_generator.py
@@ -21,7 +21,7 @@ count_halo = 0
 for j in range(nTrees):
     nh = ngalstree[j]
     for i in range(nh):
-        if(output_trees[count_halo]['FirstHaloInFOFgroup'] == i):
+        if(output_trees[count_halo]['SnapNum'] == lastsnap):
             filenr = output_trees[count_halo]['FileNr']
             cursor.execute("INSERT INTO tree(curhalonr,filenr,treenr,halonr,mass) VALUES (?,?,?,?,?)",(count_halo,int(output_trees[count_halo]['FileNr']),j,i,float(output_trees[count_halo]['M_Crit200'])))
         count_halo += 1
