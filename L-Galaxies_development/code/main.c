5a87021d78c6cfc2e3126ac2aa0defc6c65afdd1
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-06-29 23:18:41 +0100

    autoupdate

--- L-Galaxies_development/code/main.c
+++ L-Galaxies_development/code/main.c
@@ -81,6 +81,7 @@ int main(int argc, char **argv)
 
   /*Reads the parameter file, given as an argument at run time. */
   read_parameter_file(argv[1]);
+  printf("%s\n",SpecPhotDir);
 
 #ifdef MR_PLUS_MRII
   //Start with MR files and later change to MRII
