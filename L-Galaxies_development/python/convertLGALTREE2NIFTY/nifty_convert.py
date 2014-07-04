f3b95eaa5b4c351664e747fea42a4c3bc1036952
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-07-04 11:34:17 +0100

    autoupdate

--- L-Galaxies_development/python/convertLGALTREE2NIFTY/nifty_convert.py
+++ L-Galaxies_development/python/convertLGALTREE2NIFTY/nifty_convert.py
@@ -127,7 +127,6 @@ for timeid in range(len(timesnap)):
             data = data_tmp
 
         for halo in data:
-            print halo
             hid = long(halo[0])
             if hid in halomap:
                 print >> fp,  hid, len(halomap[hid])
