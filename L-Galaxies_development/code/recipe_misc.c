4570115bbfdffee2e5695e747058a4be76190fea
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-24 05:23:54 +0100

    quick commit

--- L-Galaxies_development/code/recipe_misc.c
+++ L-Galaxies_development/code/recipe_misc.c
@@ -470,7 +470,7 @@ void add_to_luminosities(int p, double mstars, double time, double metallicity)
 				    f2 * NPhotTables[metindex][0][tabindex + 1]) +
 			   fmet2 * (f1 * NPhotTables[metindex + 1][0][tabindex] +
 				    f2 * NPhotTables[metindex + 1][0][tabindex + 1]));
-      // printf("%lf\n",PhotonsToAdd);
+      printf("ori:%lf add:%lf\n",Gal[p].ReionizePhot,PhotonsToAdd);
       Gal[p].ReionizePhot += PhotonsToAdd;
 #endif // REIONIZEPHOTON
 
