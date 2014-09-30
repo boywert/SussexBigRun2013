49c337ccb7c5f2d9f33fef6f78056418a24fe294
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 06:43:16 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -10,7 +10,13 @@ lastsnap = 75
 
 firsthalointree = numpy.cumsum(ngalstree) - numpy.ones(nTrees)*ngalstree[0]
 
-for firsthalo in firsthalointree:
-    firsthalo = int(firsthalo)
-    print treedata[firsthalo]
-print treedata
+#for firsthalo in firsthalointree:
+#    firsthalo = int(firsthalo)
+#    print treedata[firsthalo]
+
+for snap in range(lastsnap):
+    for itree in range(nTrees):
+        this_treedata = treedata[firsthalointree[itree]:firsthalointree[itree]+ngalstree[itree]]
+        print ngalstree[itree], this_treedata
+
+#print treedata
