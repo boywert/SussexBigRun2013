86c52b71b8719253ee9c7cc28d25d27039ea19cc
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 06:11:59 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -2,9 +2,10 @@ import numpy
 import read_lgal
 
 folder = "/mnt/lustre/scratch/cs390/47Mpc/treedata/"
-firstfile = 127
+firstfile = 0
 lastfile = 127
 lastsnap = 75
-tree = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
+
+(nHalos,nTrees,ngalstree,treedata) = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
 
 print tree
