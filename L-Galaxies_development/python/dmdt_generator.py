a7f6d8f70956f1da6cd2462b702eb1794f165ee2
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 08:23:36 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -1,10 +1,20 @@
 import numpy
+import cosmocalc
 import read_lgal
 
 folder = "/mnt/lustre/scratch/cs390/47Mpc/treedata/"
+snap = "/mnt/lustre/scratch/cs390/47Mpc/snap.txt"
+
+a = numpy.loadtxt(snap)
+z = 1./a - 1.
+
+print z
+
 firstfile = 127
 lastfile = 127
 lastsnap = 75
+h = 0.7
+mpart = 0.000150777086098*1.e10/h  #Msun 
 
 (nHalos,nTrees,ngalstree,treedata) = read_lgal.read_lgalinput(folder,firstfile,lastfile,lastsnap)
 
@@ -14,17 +24,22 @@ firsthalointree = lasthalointree-ngalstree
 #for firsthalo in firsthalointree:
 #    firsthalo = int(firsthalo)
 #    print treedata[firsthalo]
+
+
 for itree in range(nTrees):
     this_treedata = numpy.array(treedata[firsthalointree[itree]:lasthalointree[itree]],dtype=read_lgal.struct_lgalinput)
     for snap in range(lastsnap+1):
         for igal in range(ngalstree[itree]):
-            if(this_treedata[igal]['FirstHaloInFOFgroup']== igal) & (this_treedata[igal]['SnapNum'] == snap):
-                TotalLen = this_treedata[igal]['Len']
-                nexthalo = this_treedata[igal]['NextHaloInFOFgroup'] 
-                while nexthalo > -1:
-                    print nexthalo,ngalstree[itree],'/',len(this_treedata)
-                    TotalLen += this_treedata[nexthalo]['Len']
-                    nexthalo = this_treedata[nexthalo]['NextHaloInFOFgroup'] 
-                print float(this_treedata[igal]['Len'])/TotalLen
+            if(this_treedata[igal]['SnapNum'] == snap):
+                mass_cur = this_treedata[igal]['Len']*mpart
+                # nexthalo = this_treedata[igal]['NextHaloInFOFgroup'] 
+                # while nexthalo > -1:
+                #     print nexthalo,ngalstree[itree],'/',len(this_treedata)
+                #    TotalLen += this_treedata[nexthalo]['Len']
+                #    nexthalo = this_treedata[nexthalo]['NextHaloInFOFgroup'] 
+               
+                # print float(this_treedata[igal]['Len'])/TotalLen
+                mass_prog = this_treedata[this_treedata[igal]['FirstProgenitor']]['Len']*mpart
+
 
 #print treedata
