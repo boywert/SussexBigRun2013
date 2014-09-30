23f0e4c0970cfe8b23204bb680bc767ffd3cba92
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 07:38:08 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -14,14 +14,14 @@ firsthalointree = numpy.cumsum(ngalstree) - numpy.ones(nTrees)*ngalstree[0]
 #    firsthalo = int(firsthalo)
 #    print treedata[firsthalo]
 for itree in range(nTrees):
-    this_treedata = numpy.array(treedata[firsthalointree[itree]:firsthalointree[itree]+ngalstree[itree]],dtype=read_lgal.struct_lgalinput)
+    this_treedata = numpy.array(treedata[firsthalointree[itree]:firsthalointree[itree]+ngalstree[itree]+1],dtype=read_lgal.struct_lgalinput)
     for snap in range(lastsnap+1):
         for igal in range(ngalstree[itree]):
             if(this_treedata[igal]['FirstHaloInFOFgroup']== igal) & (this_treedata[igal]['SnapNum'] == snap):
                 TotalLen = this_treedata[igal]['Len']
                 nexthalo = this_treedata[igal]['NextHaloInFOFgroup'] 
                 while nexthalo > -1:
-                    print nexthalo,ngalstree[itree]
+                    print nexthalo,ngalstree[itree],'/',len(this_treedata)
                     TotalLen += this_treedata[nexthalo]['Len']
                     nexthalo = this_treedata[nexthalo]['NextHaloInFOFgroup'] 
                 print float(this_treedata[igal]['Len'])/TotalLen
