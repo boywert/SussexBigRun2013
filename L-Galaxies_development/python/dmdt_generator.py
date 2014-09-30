ca2ee7262f8ef7b7ea0bd04b4861a4486fd5784f
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 10:05:25 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -25,7 +25,7 @@ for z in redshift_list:
     output_folder = "/mnt/lustre/scratch/cs390/47Mpc/dmdt_new/"+z_str
     os.system("mkdir -p "+output_folder)
     f.append(open(output_folder+'/'+str(this_thread),"w+"))
-    age_list.append(cosmocalc.cosmocalc(z, H0=h*100., WM=0.27, WV=0.73)['zage']*1000.) #Myr
+    age_list.append(cosmocalc.cosmocalc(z, H0=h*100., WM=0.27, WV=0.73)['zage_Gyr']*1000.) #Myr
     print cosmocalc.cosmocalc(z, H0=h*100., WM=0.27, WV=0.73)
     i += 1
 
