17e9c05dc30b6951714ce6878133ee4d9da04fa8
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 08:37:07 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -7,6 +7,10 @@ snap = "/mnt/lustre/scratch/cs390/47Mpc/snap.txt"
 
 a = numpy.loadtxt(snap)
 redshift_list = 1./a - 1.
+age_list = []
+for z in redshift_list:
+    age_list.append(cosmocalc.cosmocalc(z, H0=h*100., WM=0.27, WV=0.73)['zage'])
+
 
 firstfile = 127
 lastfile = 127
@@ -30,8 +34,7 @@ for itree in range(nTrees):
         for igal in range(ngalstree[itree]):
             if(this_treedata[igal]['SnapNum'] == snap):
                 mass_cur = this_treedata[igal]['Len']*mpart
-                z_cur = redshift_list[this_treedata[igal]['SnapNum']]
-                age_cur = cosmocalc(z_cur, H0=h*100., WM=0.27, WV=0.73)['zage']
+                age_cur = age_list[this_treedata[igal]['SnapNum']]
                 # nexthalo = this_treedata[igal]['NextHaloInFOFgroup'] 
                 # while nexthalo > -1:
                 #     print nexthalo,ngalstree[itree],'/',len(this_treedata)
@@ -40,8 +43,8 @@ for itree in range(nTrees):
                
                 # print float(this_treedata[igal]['Len'])/TotalLen
                 mass_prog = this_treedata[this_treedata[igal]['FirstProgenitor']]['Len']*mpart
-                z_prog = redshift_list[this_treedata[redshift_list[this_treedata[igal]['SnapNum']]]['SnapNum']]
-                age_prog = cosmocalc(z_prog, H0=h*100., WM=0.27, WV=0.73)['zage']
+                age_prog = age_list[this_treedata[this_treedata[igal]['FirstProgenitor']]['SnapNum']]
+
                 dm = mass_cur - mass_prog  #Msun
                 dt = age_cur-age_prog  #Gyr
 
