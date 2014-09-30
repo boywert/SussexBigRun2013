96f2c331d9113af8986c738cce41e1c7b46d683a
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 09:25:20 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -65,7 +65,7 @@ for itree in range(nTrees):
                 dm = mass_cur - mass_prog  #Msun
                 dt = (age_cur-age_prog)*1000.  #Myr
 
-                print "%g  %g  %g  %g  %g\n"%(this_treedata[igal]['Pos'][0],this_treedata[igal]['Pos'][1],this_treedata[igal]['Pos'][2],mass_cur,dm/dt),f[snap]
+                f[snap].write("%g  %g  %g  %g  %g\n"%(this_treedata[igal]['Pos'][0],this_treedata[igal]['Pos'][1],this_treedata[igal]['Pos'][2],mass_cur,dm/dt))
 
 
 #print treedata
