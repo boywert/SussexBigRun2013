98e11c4cb2c796bc3275907dfadc231d20960702
Chaichalit Srisawat <boywert@Chaichalits-MacBook-Pro.local>
2014-09-30 09:15:59 +0100

    quick commit

--- L-Galaxies_development/python/dmdt_generator.py
+++ L-Galaxies_development/python/dmdt_generator.py
@@ -1,11 +1,16 @@
 import numpy
 import cosmocalc
 import read_lgal
+import os
+import sys
 
 folder = "/mnt/lustre/scratch/cs390/47Mpc/treedata/"
 snap = "/mnt/lustre/scratch/cs390/47Mpc/snap.txt"
-firstfile = 127
-lastfile = 127
+
+this_thread = 127
+
+firstfile = this_thread
+lastfile = this_thread
 lastsnap = 75
 h = 0.7
 mpart = 0.000150777086098*1.e10/h  #Msun 
@@ -13,8 +18,15 @@ mpart = 0.000150777086098*1.e10/h  #Msun
 a = numpy.loadtxt(snap)
 redshift_list = 1./a - 1.
 age_list = []
+f = []
 for z in redshift_list:
+    z_str="%.3f"%(z)
+    folder = "/mnt/lustre/scratch/cs390/47Mpc/dmdt_new/"+z_str
+    os.system("mkdir -p "+folder)
+    f.append(open(folder+'/'+str(this_thread)))
     age_list.append(cosmocalc.cosmocalc(z, H0=h*100., WM=0.27, WV=0.73)['zage'])
+
+
 
 struct_dmdt = numpy.dtype([
     ('Mass',numpy.float32,1),
@@ -30,6 +42,7 @@ firsthalointree = lasthalointree-ngalstree
 #for firsthalo in firsthalointree:
 #    firsthalo = int(firsthalo)
 #    print treedata[firsthalo]
+
 
 
 for itree in range(nTrees):
@@ -52,7 +65,7 @@ for itree in range(nTrees):
                 dm = mass_cur - mass_prog  #Msun
                 dt = (age_cur-age_prog)*1000.  #Myr
 
-                print "%g  %g  %g  %g  %g\n"%(this_treedata[igal]['Pos'][0],this_treedata[igal]['Pos'][1],this_treedata[igal]['Pos'][2],mass_cur,dm/dt)
+                print "%g  %g  %g  %g  %g\n"%(this_treedata[igal]['Pos'][0],this_treedata[igal]['Pos'][1],this_treedata[igal]['Pos'][2],mass_cur,dm/dt),f[snap]
 
 
 #print treedata
