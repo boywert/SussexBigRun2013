e0ab61d3b66a38fd5cb8eaef34b13239ff887498
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-09 10:30:28 +0100

    quick commit

--- L-Galaxies_development/python/gen_source.c
+++ L-Galaxies_development/python/gen_source.c
@@ -9,13 +9,16 @@ int main()
   struct LGalaxy *lgal;
   FILE *fp;
   double *Sfr;
+  int cubep3m_p = 1728;
   float boxsize = 47.0;
   int grid = 306;
   float gridsize = boxsize/grid;
+  int cubep3m_cell = cubep3m_p*2;
   long long cell;
   int firstfile = 0;
   int lastfile = 127;
   char *basename = "/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/SA_z";
+  char *outputfolder = "/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/sources/";
   char *zlistfile = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt";
   char zlist_string[100][1024];
   char filename[2048];
@@ -24,7 +27,26 @@ int main()
   int dummy,*dummyarray;
   int i,j,k;
   int nSnaps;
+  float gridmass;
+
+  float Mpc2m = 3.08567758e22;
+  float m2Mpc = 1./Mpc2m;
+  float Msun2kg = 1.98855e30;
+  float kg2Msun = 1./Msun2kg;
+  float m2km = 0.001;
+  float omegam = 0.27;
+  float G = 6.674e-11;   // SI
+  float h = 0.7;
+  float H0 = 100.0;        // km/s / (Mpc/h)
+  float pi = 4.0*atan(1.0);
+  float rho_crit_0;
   
+  float gridmass_c; //to convert msun to gridmass 
+  
+  G *= (m2km*m2km) * (m2Mpc) / (kg2Msun); //  (Mpc/h) (km/s)^2 / (Msun/h)
+  rho_crit_0 = 3.* (H0*H0)/ (8.*pi*G); //  # (1e10 Msun/h)/(Mpc/h)^3
+  gridmass = omegam*rho_crit_0*(boxsize*boxsize*boxsize)/(cubep3m_cell*cubep3m_cell*cubep3m_cell)/h; // Msun
+  gridmass_c = 1./gridmass
   fp = fopen(zlistfile,"r");
   i=0;
   while (fscanf(fp, "%s", zlist_string[i]) != EOF) {
@@ -46,9 +68,13 @@ int main()
       fread(lgal,sizeof(struct LGalaxy),nGals,fp);
       for(k=0;k<nGals;k++) {
 	cell = lgal[k].Pos[0]/gridsize + lgal[k].Pos[1]/gridsize*grid + lgal[k].Pos[2]/gridsize*grid*grid;
-	Sfr[cell] += lgal[k].Sfr;
+	Sfr[cell] += lgal[k].Sfr*gridmass_c;
       }
       free(lgal);
+      fclose(fp);
+    }
+    for(i=0;i<100;i++){
+      printf("%d %lf\n",i,Sfr[i]); 
     }
     free(Sfr);
   }
