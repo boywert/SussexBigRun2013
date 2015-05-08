b499bf7728d10fa1025689c716e78eef5d21bc9d
Chaichalit Srisawat <boywert@gmail.com>
2015-05-08 16:09:42 +0100

    quick commit

--- L-Galaxies_development/code/model_spectro_photometric_onthefly_initialize.c
+++ L-Galaxies_development/code/model_spectro_photometric_onthefly_initialize.c
@@ -197,7 +197,7 @@ void read_InputSSP_spectra(double LambdaInputSSP[SSP_NAGES][SSP_NLambda], double
   			SSP_logAgeTab[ageloop]=log10(age / 1.0e6 / UnitTime_in_Megayears * Hubble_h);
   		else
   			SSP_logAgeTab[ageloop]=0.;
-		printf("ageloop = %d age = %g\n",ageloop,SSP_logAgeTab);
+		printf("ageloop = %d age = %g\n",ageloop,SSP_logAgeTab[ageloop]);
 	}
   }
 
